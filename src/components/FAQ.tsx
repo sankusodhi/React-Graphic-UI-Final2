39f75b1daedbebae85bf5a35688feb0897ab6573
BimleshMandavi <bimleshmandavi0@gmail.com>
2025-01-13 13:30:06 +0530

    fix:all sections

--- src/components/FAQ.tsx
+++ src/components/FAQ.tsx
@@ -1,3 +1,66 @@
+// "use client";
+
+// import { useState } from 'react';
+// import { motion, AnimatePresence } from 'framer-motion';
+
+// const questions = [
+//   { id: 1, question: 'Question', answer: 'Answer' },
+//   { id: 2, question: 'Question', answer: 'Answer' },
+//   { id: 3, question: 'Question', answer: 'Answer' },
+//   { id: 4, question: 'Question', answer: 'Answer' },
+//   { id: 5, question: 'Question', answer: 'Answer' },
+//   { id: 6, question: 'Closed Question Example', answer: '' },
+// ];
+
+// export default function FAQ() {
+//   // Explicitly define state type as number or null
+//   const [openQuestion, setOpenQuestion] = useState<number | null>(null);
+
+//   // Ensure 'id' parameter has a defined type
+// const toggleQuestion = (id: number) => {
+//   setOpenQuestion(openQuestion === id ? null : id);
+// };
+
+//   return (
+//     <div className="w-full mx-auto pt-16 px-4 sm:px-6 pb-16 md:px-28 bg-[#ffffff]">
+//       <h1 className="text-6xl  text-start mb-6">
+//         Resolve your Doubts here.
+//       </h1>
+//       <ul className="space-y-8 sm:pt-14 sm:text-5xl">
+//         {questions.map(({ id, question, answer }) => (
+//           <li key={id} className="border-b">
+//             <div
+//               className="flex items-center text-center justify-between py-2  cursor-pointer"
+//               onClick={() => toggleQuestion(id)}
+//             >
+//               <span className="text-lg mb-8 font-semibold  break-words sm:text-3xl">
+//                 {id}. {question}
+//               </span>
+//               <button className="text-white font-bold text-xl text-center sm:text-3xl lg:text-3xl rounded-full w-10 h-10 bg-[#1600ea] t">
+//                 {openQuestion === id ? '×' : '+'}
+//               </button>
+//             </div>
+//             <AnimatePresence>
+//               {openQuestion === id && (
+//                 <motion.div
+//                   initial={{ opacity: 0, height: 0 }}
+//                   animate={{ opacity: 1, height: 'auto' }}
+//                   exit={{ opacity: 0, height: 0 }}
+//                   className="overflow-hidden"
+//                 >
+//                   <p className="py-2 mb-4 px-4 text-gray-600 text-sm sm:text-lg lg:text-lg">
+//                     {answer}
+//                   </p>
+//                 </motion.div>
+//               )}
+//             </AnimatePresence>
+//           </li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// }
+
 "use client";
 
 import { useState } from 'react';
@@ -36,7 +99,7 @@ const toggleQuestion = (id: number) => {
               <span className="text-lg mb-8 font-semibold  break-words sm:text-3xl">
                 {id}. {question}
               </span>
-              <button className="text-white font-bold text-xl text-center sm:text-3xl lg:text-3xl rounded-full w-10 h-10 bg-[#1600ea] t">
+              <button className="text-white font-bold text-xl text-center sm:text-3xl lg:text-3xl sm:mb-10 rounded-full w-12 h-12 bg-[#1600ea] t">
                 {openQuestion === id ? '×' : '+'}
               </button>
             </div>
--- src/components/Statistics.tsx
+++ src/components/Statistics.tsx
@@ -1,16 +1,230 @@
+// "use client";
+
+// import { motion } from 'framer-motion';
+
+
+// export default function BenefitSection() {
+//   return (
+  
+//     <div className="bg-[#1600ea] text-white sm:py-12 sm:px-8 pt-9">
+
+//       {/* Benefit Section */}
+//       <div className="mb-10 sm:">
+//         <h2 className="text-xl md:text-6xl ">How we will benefit you.</h2>
+//       </div>
+
+//       {/* Grid Section */}
+//       <div className=" md:grid-cols-2 p-6 lg:grid-cols-3 gap-8 md:p-20">
+//         {/* Card 1 */}
+//         <motion.div
+//           whileHover={{ scale: 1.05 }}
+//           className="bg-white text-black  p-4 sm:p-0 rounded-lg shadow-lg sm:flex  sm::mx-28"
+//         >
+         
+//           <div className='sm:w-[70%] sm:p-20 '>
+//           <h3 className="text-4xl font-bold font-Roboto mb-2 sm:mt-4 sm:mb-10">Hire Us or On-board Top 1% Creative Talent ✨</h3>
+//           <p className="text-xl mb-4">
+//             Collaborate with our expert team for tailored, high-quality
+// designs aligned with your brand’s goals, or onboard top
+// creative talent for fresh perspectives. Our flexible approach
+// ensures impactful, client-focused solutions that resonate
+// with your audience.
+//           </p>
+//            <a href="#" className="text-[#1600ea] font-medium">Hire Designers →</a>
+//            </div>
+//            <div className="text-center pt-10 ">
+//             <img className=''
+//                 src="/pic-15.png"
+//                 alt="Arjun Mehta"
+
+//               />
+
+//             </div>
+
+//         </motion.div>
+
+//         {/* Card 2 */}
+//         <div className='flex justify-evenly mt-12 sm:mt-0 w-full gap-5 md:gap-10 p-4 sm:p-0 '>
+//         <div className='sm:flex sm:justify-evenly  sm:mt-12 sm:w-full sm:gap-5 md:gap-10'>
+//         <motion.div
+//           whileHover={{ scale: 1.05 }}
+//           className="bg-white text-black   rounded-lg shadow-lg"
+//         >
+          
+//           <div className='p-6 mt-6'>
+//           <h3 className="text-4xl font-bold mb-2">Creatives that Drives Revenue 💰</h3>
+//           <p className="text-xl mb-4">
+//             Strategically crafted concepts that captivate engagement and boost conversions to maximize profitability.
+//           </p>
+//           <a href="#" className="text-[#1600ea] font-medium"> Know More →</a>
+//           </div>
+//           <div className="text-center ">
+           
+//             <img className='px-8 pt-[19.8px]'
+//                 src="/pic-03.png"
+//                 alt="Arjun Mehta"
+
+
+//               />
+
+//             </div>
+//         </motion.div>
+
+//         {/* Card 3 */}
+//         <motion.div
+//           whileHover={{ scale: 1.05 }}
+//           className="bg-white text-black rounded-lg shadow-lg"
+//         >
+//           <div className='p-6'>
+//           <h3 className="text-4xl font-bold mb-2">Top-notch quality, consistently delivered with excellence 🤩</h3>
+//           <p className="text-xl mb-4">
+//             Scale your content with precisely crafted visuals that ensure consistency
+// and lasting impact, seamlessly aligning with your brand to create engaging,
+// resonant results.
+//           </p>
+//           <a href="#" className="text-[#1600ea] font-medium">Know More →</a>
+//           </div>
+//           <div className="text-center sm:pt-[78px]">
+      
+//             <img className='sm:h-full  sm:pl-[24px] rounded-lg'
+//                 src="/pic-17.png"
+//                 alt="Arjun Mehta"
+
+
+//               />
+
+//             </div>
+//         </motion.div>
+//         </div>
+//       </div>
+//     </div>
+//     </div>
+//   );
+// }
+
+// "use client";
+
+// import { motion } from 'framer-motion';
+
+
+// export default function BenefitSection() {
+//   return (
+
+//     <div className="bg-[#1600ea] text-white sm:py-12 sm:px-8 pt-9">
+
+//       {/* Benefit Section */}
+//       {/* <div className="text-center mt-10"> */}
+//       <div className="text-center mt-10">
+//         <h2 className="text-xl md:text-6xl ">How we will benefit you.</h2>
+//       </div>
+
+//       {/* Grid Section */}
+//       <div className=" md:grid-cols-2 p-6 lg:grid-cols-3 gap-8 md:p-20">
+//         {/* Card 1 */}
+//         <motion.div
+//           whileHover={{ scale: 1.05 }}
+//           className="bg-white text-black  p-4 sm:p-0 rounded-lg shadow-lg sm:flex  sm::mx-28"
+//         >
+
+//           <div className='sm:w-[70%] sm:p-20 '>
+//           <h3 className="text-4xl font-bold mb-2 sm:mt-4 sm:mb-10">Hire Us or On-board Top 1% Creative Talent ✨</h3>
+//           <p className="text-xl mb-4">
+//             Collaborate with our expert team for tailored, high-quality
+// designs aligned with your brand’s goals, or onboard top
+// creative talent for fresh perspectives. Our flexible approach
+// ensures impactful, client-focused solutions that resonate
+// with your audience.
+//           </p>
+//            <a href="#" className="text-[#1600ea] font-medium">Hire Designers →</a>
+//            </div>
+//            <div className="text-center pt-10 ">
+//             <img className=''
+//                 src="/pic-15.png"
+//                 alt="Arjun Mehta"
+
+//               />
+
+//             </div>
+
+//         </motion.div>
+
+//         {/* Card 2 */}
+//         <div className='flex justify-evenly mt-12 sm:mt-0 w-full gap-5 md:gap-10 p-4 sm:p-0 '>
+//         <div className='sm:flex sm:justify-evenly  sm:mt-12 sm:w-full sm:gap-5 md:gap-10'>
+//         <motion.div
+//           whileHover={{ scale: 1.05 }}
+//           className="bg-white text-black   rounded-lg shadow-lg"
+//         >
+
+//           <div className='p-6 mt-6'>
+//           <h3 className="text-4xl font-bold mb-2">Creatives that Drives Revenue 💰</h3>
+//           <p className="text-xl mb-4">
+//             Strategically crafted concepts that captivate engagement and boost conversions to maximize profitability.
+//           </p>
+//           <a href="#" className="text-[#1600ea] font-medium"> Know More →</a>
+//           </div>
+//           <div className="text-center ">
+
+//             <img className='px-8 pt-[19.8px]'
+//                 src="/pic-03.png"
+//                 alt="Arjun Mehta"
+
+
+//               />
+
+//             </div>
+//         </motion.div>
+
+//         {/* Card 3 */}
+//         <motion.div
+//           whileHover={{ scale: 1.05 }}
+//           className="bg-white text-black rounded-lg shadow-lg"
+//         >
+//           <div className='p-6'>
+//           <h3 className="text-4xl font-bold mb-2">Top-notch quality, consistently delivered with excellence 🤩</h3>
+//           <p className="text-xl mb-4">
+//             Scale your content with precisely crafted visuals that ensure consistency
+// and lasting impact, seamlessly aligning with your brand to create engaging,
+// resonant results.
+//           </p>
+//           <a href="#" className="text-[#1600ea] font-medium">Know More →</a>
+//           </div>
+//           <div className="text-center sm:pt-[78px]">
+
+//             <img className='sm:h-full  sm:pl-[24px] rounded-lg'
+//                 src="/pic-17.png"
+//                 alt="Arjun Mehta"
+
+
+//               />
+
+//             </div>
+//         </motion.div>
+//         </div>
+//       </div>
+//     </div>
+//     </div>
+//   );
+// }
+
+
+
+
 "use client";
 
 import { motion } from 'framer-motion';
 
+// import { LuArrowUpRight } from "react-icons/lu";
 
 export default function BenefitSection() {
   return (
-  
-    <div className="bg-[#1600ea] text-white sm:py-12 sm:px-8 pt-9">
+
+    <div className="bg-[#1600ea] text-white sm:py-4  sm:px-8 sm:pt-16">
 
       {/* Benefit Section */}
-      <div className="mb-10 sm:">
-        <h2 className="text-xl md:text-6xl ">How we will benefit you.</h2>
+      {/* <div className="text-center mt-10"> */}
+      <div className=" sm:ml-20 mt-12">
+        <h2 className="text-xl md:text-6xl sm:text-5xl ">How we will benefit you.</h2>
       </div>
 
       {/* Grid Section */}
@@ -20,84 +234,95 @@ export default function BenefitSection() {
           whileHover={{ scale: 1.05 }}
           className="bg-white text-black  p-4 sm:p-0 rounded-lg shadow-lg sm:flex  sm::mx-28"
         >
-         
-          <div className='sm:w-[70%] sm:p-20 '>
-          <h3 className="text-4xl font-bold font-Roboto mb-2 sm:mt-4 sm:mb-10">Hire Us or On-board Top 1% Creative Talent ✨</h3>
-          <p className="text-xl mb-4">
-            Collaborate with our expert team for tailored, high-quality
-designs aligned with your brand’s goals, or onboard top
-creative talent for fresh perspectives. Our flexible approach
-ensures impactful, client-focused solutions that resonate
-with your audience.
-          </p>
-           <a href="#" className="text-[#1600ea] font-medium">Hire Designers →</a>
-           </div>
-           <div className="text-center pt-10 ">
-            <img className=''
-                src="/pic-15.png"
-                alt="Arjun Mehta"
 
-              />
-
+          <div className='sm:w-[70%]  '>
+            <h3 className="text-4xl font-bold sm:mt-16 sm:px-14  ">Hire Us or On-board</h3>
+            <h3 className="text-4xl font-bold  sm:px-14  sm:mb-8">Top 1% Creative Talent ✨</h3>
+            <p className="text-xl sm:px-14">
+              Collaborate with our expert team for tailored, high-quality
+              designs aligned with your brand’s goals, or onboard top
+              creative talent for fresh perspectives. Our flexible approach
+              ensures impactful, client-focused solutions that resonate
+              with your audience.
+            </p>
+            <div className='p-6'>
+              {/* <a href="#" className="text-[#1600ea] sm:px-8 sm:mt-16 font-medium">Hire Designers <LuArrowUpRight /> */}
+              <a href="#" className="text-[#1600ea] sm:px-8 sm:mt-16 font-medium">Hire Designers &#x2197;</a>
             </div>
+          </div>
+          <div className="text-center pt-10 ">
+            <img className='h-[380px] w-[1000px]'
+              src="/pic-15.png"
+              alt="Arjun Mehta"
+
+            />
+
+          </div>
 
         </motion.div>
 
         {/* Card 2 */}
         <div className='flex justify-evenly mt-12 sm:mt-0 w-full gap-5 md:gap-10 p-4 sm:p-0 '>
-        <div className='sm:flex sm:justify-evenly  sm:mt-12 sm:w-full sm:gap-5 md:gap-10'>
-        <motion.div
-          whileHover={{ scale: 1.05 }}
-          className="bg-white text-black   rounded-lg shadow-lg"
-        >
-          
-          <div className='p-6 mt-6'>
-          <h3 className="text-4xl font-bold mb-2">Creatives that Drives Revenue 💰</h3>
-          <p className="text-xl mb-4">
-            Strategically crafted concepts that captivate engagement and boost conversions to maximize profitability.
-          </p>
-          <a href="#" className="text-[#1600ea] font-medium"> Know More →</a>
+          <div className='sm:flex sm:justify-evenly  sm:mt-12 sm:w-full sm:gap-5 md:gap-10'>
+            <motion.div
+              whileHover={{ scale: 1.05 }}
+              className="bg-white text-black   rounded-lg shadow-lg"
+            >
+
+              <div className=' mt-1'>
+                <h3 className="text-4xl font-bold sm:mt-9  sm:px-14 mb-2">Creatives that</h3>
+                <h3 className="text-4xl font-bold  sm:px-14 mb-2"> Drives Revenue 💰</h3>
+                <p className="text-xl text-justify  sm:px-14 ">
+                  Strategically crafted concepts that captivate engagement and boost conversions to maximize profitability.
+                </p>
+                <div className='p-6'>
+                  <a href="#" className="text-[#1600ea]  sm:px-8 font-medium"> Know More  &#x2197;</a>
+                </div>
+              </div>
+              <div className=" sm:px-14 ">
+
+                <img className=' h-[345px] '
+                  src="/pic-03.png"
+                  alt="Arjun Mehta"
+
+
+                />
+
+              </div>
+            </motion.div>
+
+            {/* Card 3 */}
+            <motion.div
+              whileHover={{ scale: 1.05 }}
+              className="bg-white text-black rounded-lg shadow-lg"
+            >
+              <div className=''>
+                <h3 className="text-4xl font-bold sm:mt-10 sm:px-14 mb-2">Top-notch quality, consistently delivered with excellence 🤩</h3>
+                <p className="text-xl sm:px-14">
+                  Scale your content with precisely crafted visuals that ensure consistency
+                  and lasting impact, seamlessly aligning with your brand to create engaging,
+                  resonant results.
+                </p>
+                <div className='p-6'>
+
+                  <a href="#" className="text-[#1600ea] sm:px-8  font-medium">Know More  &#x2197;</a>
+
+                </div>
+              </div>
+              <div className="text-center sm:pl-14 ">
+
+                <img className='rounded-lg'
+                  src="/pic-17.png"
+                  alt="Arjun Mehta"
+
+
+                />
+
+              </div>
+            </motion.div>
           </div>
-          <div className="text-center ">
-           
-            <img className='px-8 pt-[19.8px]'
-                src="/pic-03.png"
-                alt="Arjun Mehta"
-
-
-              />
-
-            </div>
-        </motion.div>
-
-        {/* Card 3 */}
-        <motion.div
-          whileHover={{ scale: 1.05 }}
-          className="bg-white text-black rounded-lg shadow-lg"
-        >
-          <div className='p-6'>
-          <h3 className="text-4xl font-bold mb-2">Top-notch quality, consistently delivered with excellence 🤩</h3>
-          <p className="text-xl mb-4">
-            Scale your content with precisely crafted visuals that ensure consistency
-and lasting impact, seamlessly aligning with your brand to create engaging,
-resonant results.
-          </p>
-          <a href="#" className="text-[#1600ea] font-medium">Know More →</a>
-          </div>
-          <div className="text-center sm:pt-[78px]">
-      
-            <img className='sm:h-full  sm:pl-[24px] rounded-lg'
-                src="/pic-17.png"
-                alt="Arjun Mehta"
-
-
-              />
-
-            </div>
-        </motion.div>
         </div>
       </div>
-    </div>
     </div>
   );
 }
