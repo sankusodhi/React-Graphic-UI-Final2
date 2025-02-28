5036e1a1b813b12d94fd49bdafa86f493dfc26b8
Bimlesh Mandavi <bimleshmandavi0@gmail.com>
2025-02-28 17:19:19 +0530

    add scroll at servise

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -138,7 +138,7 @@ export default function TakeAdvantageSection() {
         >
           Take Advantage
         </motion.h2>
-        <div className="grid grid-cols-1  sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3  gap-6 px-4 w-full sm:mr-4">
+        <div className="grid grid-cols-1  sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3  gap-6 px-4 w-full sm:mr-4 sm:h-[100vh] overflow-y-scroll">
           {items.slice(0, visibleCount).map((item, index) => (
             <motion.div
               key={index}
